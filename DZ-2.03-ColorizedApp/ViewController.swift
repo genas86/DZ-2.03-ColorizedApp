761191dbbf51f62ef3c3ef9c6740e7125420952e
genas86 <sgm86@bk.ru>
2022-08-02 16:58:21 +0300

    Initial Commit

---
+++ DZ-2.03-ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZ-2.03-ColorizedApp
+//
+//  Created by Genas1986 on 02.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
